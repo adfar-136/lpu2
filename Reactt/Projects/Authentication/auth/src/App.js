668c2613975b4bf1028a01d43d03187c72992f5b
adfar-136 <adfarrasheed136@gmail.com>
2023-02-05 11:29:49 +0530

    done

--- Reactt/Projects/Authentication/auth/src/App.js
+++ Reactt/Projects/Authentication/auth/src/App.js
@@ -1,9 +1,21 @@
 
 import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './Components/Home/Home'
+import Login from './Components/Login/Login'
+import SignUp from './Components/Signup/SignUp'
 
 export default function App() {
   return (
-    <div>App</div>
+    <div>
+      <BrowserRouter>
+       <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/signup' element={<SignUp/>}/>
+       </Routes>
+      </BrowserRouter>
+    </div>
   )
 }
 
---
+++ Reactt/Projects/Authentication/auth/src/Components/Signup/SignUp.js
@@ -0,0 +1,60 @@
+
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import { auth } from '../../firebase'
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import InputForm from '../InputForm/InputForm'
+import styles from './SignUp.module.css'
+
+export default function SignUp() {
+    const [errorMsg,setErrorMsg] = useState("")
+    const [values,setValues] = useState({
+        name:"",
+        email:"",
+        pass:""
+    })
+    const [submitButtonDisabled,setsubmitButtonDisabled] = useState(false)
+    const navigate = useNavigate()
+    const handleSubmission=()=>{
+        if(!values.name || !values.email || !values.pass){
+            setErrorMsg("Fill All Fields")
+            return;
+        }
+        setErrorMsg("")
+        setsubmitButtonDisabled(true)
+        createUserWithEmailAndPassword(auth,values.email,values.pass)
+        .then((res)=>{
+            const user= res.user
+            console.log(user)
+            updateProfile(user,{
+                displayName:values.name,
+            })
+            navigate('/')
+        }).catch((err)=>{
+            setsubmitButtonDisabled(false)
+            setErrorMsg(err.message)
+        })
+    }
+  return (
+    <div className={styles.container}>
+        <div className={styles.innerBox}>
+            <h1 className={styles.heading}>SignUp</h1>
+             <InputForm type="text" label="Name:" placeholder="Enter Your Name" 
+             onChange={(event)=>setValues((prev)=>({...prev,name:event.target.value}))}/>
+             <InputForm type="email" label="Email" placeholder="Enter Your Email"
+             onChange={(event)=>setValues((prev)=>({...prev,email:event.target.value}))}/>
+             <InputForm type="password" label="Password" placeholder="Enter Your Password"
+             onChange={(event)=>setValues((prev)=>({...prev,pass:event.target.value}))}/>
+             <div className={styles.footer}>
+               <b className={styles.error}>{errorMsg}</b>
+               <button onClick={handleSubmission} disabled={submitButtonDisabled}>SignUp</button>
+               <p>Already have an account? {" "}</p>
+               <span>
+                <Link to='/login'>Login</Link>
+               </span>
+             </div>
+        </div>
+        
+    </div>
+  )
+}
